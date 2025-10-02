7ed7eab7a9c7651cc27f5e8bc38a73b8e633f948
Tatyana Garcés <angiegarc@unicauca.edu.co>
2025-10-02 15:12:24 -0500

    Agregada funcion de guardado de factura

---
+++ facturacion/crear_factura.php
@@ -0,0 +1,18 @@
+<?php
+include '../conexion.php';
+
+$id_cliente = $_POST['id_cliente'];
+$total = $_POST['total'];
+$fecha = $_POST['fecha'];
+
+$sql = "INSERT INTO factura (id_cliente, total, fecha)
+        VALUES ($id_cliente, $total, '$fecha')";
+
+$result_save = $conexion->query($sql);
+if ($result_save) {
+    echo json_encode($conexion->insert_id);
+    // echo "Cliente registrado con éxito";
+} else {
+    echo "Error: " . $conexion->error;
+}
+?>
